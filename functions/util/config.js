8e1699c389902f267bf81d2c006d08929e7acb6d
srux <samrob.nz@gmail.com>
2020-01-14 13:24:38 +1300

    function setup

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyDsh-_e5ngHkqdOP0aT5Da7JpB85iEFiGE",
+  authDomain: "serverlessfirebase.firebaseapp.com",
+  databaseURL: "https://serverlessfirebase.firebaseio.com",
+  projectId: "serverlessfirebase",
+  storageBucket: "serverlessfirebase.appspot.com",
+  messagingSenderId: "267661224605",
+  appId: "1:267661224605:web:cd07abf831e959685e4274"
+};
